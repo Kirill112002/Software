a5d9d7c5a3b36344a1f455dcacd7a733f153adb9
Kirill112002 <kirill_antonov1111@mail.ru>
2023-01-07 01:14:30 +0300

    Add Abstract Servlets

---
+++ HW3-Refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/MethodServlet.java
@@ -0,0 +1,33 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+public abstract class MethodServlet extends Servlet {
+
+    protected abstract String getQuery(Parameters parameters);
+
+    protected String getInvalidAnswer() {
+        return null;
+    }
+
+    protected abstract void updateResponse(Parameters parameters, ResultSet resultSet, HttpServletResponse response) throws IOException, SQLException;
+
+    @Override
+    protected void statementGet(Parameters parameters, Statement statement, HttpServletResponse response) throws SQLException, IOException {
+        String query = getQuery(parameters);
+        if (query == null) {
+            response.getWriter().println("<html><body>");
+            String invalidAnswer = getInvalidAnswer();
+            if (invalidAnswer != null) {
+                response.getWriter().println(invalidAnswer);
+            }
+            response.getWriter().println("</body></html>");
+        } else {
+            ResultSet resultSet = statement.executeQuery(getQuery(parameters));
+            updateResponse(parameters, resultSet, response);
+            resultSet.close();
+        }
+    }
+}
--- HW3-Refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ HW3-Refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -1,112 +1,74 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * @author akirakozov
  */
-public class QueryServlet extends HttpServlet {
+public class QueryServlet extends MethodServlet {
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String command = request.getParameter("command");
-
-        if ("max".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("<h1>Product with max price: </h1>");
-
-                    while (rs.next()) {
-                        String name = rs.getString("name");
-                        int price = rs.getInt("price");
-                        response.getWriter().println(name + "\t" + price + "</br>");
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("min".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("<h1>Product with min price: </h1>");
-
-                    while (rs.next()) {
-                        String name = rs.getString("name");
-                        int price = rs.getInt("price");
-                        response.getWriter().println(name + "\t" + price + "</br>");
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("sum".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("Summary price: ");
-
-                    if (rs.next()) {
-                        response.getWriter().println(rs.getInt(1));
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("count".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("Number of products: ");
-
-                    if (rs.next()) {
-                        response.getWriter().println(rs.getInt(1));
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            response.getWriter().println("Unknown command: " + command);
+    protected String getQuery(Parameters parameters) {
+        String command = parameters.parameters.get("command")[0];
+        switch (command) {
+            case "max":
+                return "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+            case "min":
+                return "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+            case "sum":
+                return "SELECT SUM(price) FROM PRODUCT";
+            case "count":
+                return "SELECT COUNT(*) FROM PRODUCT";
+            default:
+                return null;
         }
+    }
 
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+    @Override
+    protected String getInvalidAnswer() {
+        return "Invalid command";
+    }
+
+    @Override
+    protected void updateResponse(Parameters parameters, ResultSet resultSet, HttpServletResponse response) throws IOException, SQLException {
+        String command = parameters.parameters.get("command")[0];
+        response.getWriter().println("<html><body>");
+        switch (command) {
+            case "max":
+                response.getWriter().println("<h1>Product with max price: </h1>");
+                while (resultSet.next()) {
+                    String name = resultSet.getString("name");
+                    int price = resultSet.getInt("price");
+                    response.getWriter().println(name + "\t" + price + "</br>");
+                }
+                break;
+            case "min":
+                response.getWriter().println("<h1>Product with min price: </h1>");
+                while (resultSet.next()) {
+                    String name = resultSet.getString("name");
+                    int price = resultSet.getInt("price");
+                    response.getWriter().println(name + "\t" + price + "</br>");
+                }
+                break;
+            case "sum":
+                response.getWriter().println("Summary price: ");
+                if (resultSet.next()) {
+                    response.getWriter().println(resultSet.getInt(1));
+                }
+                break;
+            case "count":
+                response.getWriter().println("Number of products: ");
+                if (resultSet.next()) {
+                    response.getWriter().println(resultSet.getInt(1));
+                }
+                break;
+            default:
+                throw new IllegalStateException("Getting response from invalid parameters");
+        }
+        response.getWriter().println("</body></html>");
     }
 
 }
