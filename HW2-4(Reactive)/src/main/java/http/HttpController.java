bf6e2c7b10e5af31bf4ebdb9f8b56281346f04bc
Kirill112002 <kirill_antonov1111@mail.ru>
2023-04-10 10:30:56 +0300

    revert commit

--- HW2-4(Reactive)/src/main/java/http/HttpController.java
+++
@@ -1,8 +0,0 @@
-package http;
-
-import io.reactivex.netty.protocol.http.server.HttpServerRequest;
-import rx.Observable;
-
-public interface HttpController {
-    <T> Observable<String> getResponse(HttpServerRequest<T> request);
-}
